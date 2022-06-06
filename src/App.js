dd697aa6d8dd3ba49fd006844456ec32952ac70e
Marc Sednaoui <marcsednaoui@hotmail.com>
2022-06-06 16:43:43 +0200

    create candide langing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import logo from './assets/logo.png';
+import twitterLogo from './assets/twitter.svg';
+import githubLogo from './assets/github.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="logo" />
+        <h1>
+          CANDIDE
+        </h1>
+        {/* increase image size */}
+        <div className="social-links">
+          <a href="https://twitter.com/candidewallet" target="_blank" rel="noopener noreferrer">
+            <img src={twitterLogo} alt="twitter" />
+          </a>
+          <a href="https://github.com/Sednaoui/Candide/tree/main/wallet" target="_blank" rel="noopener noreferrer">
+            <img src={githubLogo} alt="github" />
+          </a>
+        </div>
       </header>
     </div>
   );
